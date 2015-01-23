73360ab4b93d48c7a3362cb7eb795783afdecb67
Michael Savin <my.savin@yandex.com>
2015-01-23 13:47:12 +0200

    Delete usage from factory.rb

--- factory.rb
+++ factory.rb
@@ -26,16 +26,3 @@ class Factory
 end
 
 
-		Costumer = Factory.new(:name, :size, :cost) do
-			def hello
-				puts "Hello, i'm #{name}."
-			end
-		end
-
-		costumer = Costumer.new("Mike", "big", "$100")
-
-		puts costumer[0]
-
-		costumer.hello
-
-
